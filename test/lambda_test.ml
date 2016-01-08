15c62a786829e5c33a77c9281c055df9cd47ff75
oskimura <oskimura@oskimura-virtual-machine.(none)>
2016-01-08 17:05:09 +0900

    add tests

--- test/lambda_test.ml
+++ test/lambda_test.ml
@@ -1,3 +1,16 @@
 open OUnit
 
 open Lambda
+
+let t1 =  TmVar({line = 1}, 1, 1)
+let ta1 = TmAbs({line=1},"x",t1)
+
+let _ = run_test_tt_main begin "lambda.ml" >::: [
+  "isval" >:: begin fun () ->
+    assert_equal (isval [] ta1) true
+  end;
+  "findlist" >:: begin fun () ->
+     assert_equal (pickfreshname [] "x") ([("x", 0)], "x0")
+  end
+] 
+end
